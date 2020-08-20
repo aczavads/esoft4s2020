b379456e44939714bc09f7b8caf5f19a430dc87a
leonardo <leonardo.rs.2105@gmail.com>
2020-08-20 20:36:32 -0300

    commit sei la, inicial
    inicio de classe e tal

---
+++ src/main/java/aLeonardoROdriguesSouza/aula20200820/App.java
@@ -0,0 +1,16 @@
+package aLeonardoROdriguesSouza.aula20200820;
+
+public class App {
+
+  public static void main(String[] args) {
+    for (int i = 0; i < 3; i++) {
+
+      System.out.println("hello mundo");
+
+    }
+    Carro fusca = new Carro();
+    fusca.ligar();
+    fusca.desligar();
+
+  }
+}
---
+++ src/main/java/aLeonardoROdriguesSouza/aula20200820/Carro.java
@@ -0,0 +1,12 @@
+package aLeonardoROdriguesSouza.aula20200820;
+
+public class Carro {
+    public void ligar(){        
+        System.out.println("Ligando..");
+    }
+    
+    public void desligar(){
+        System.out.println("Desligando..");
+    }
+
+}
